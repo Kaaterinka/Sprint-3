5c528efb1482322103bf0fee2d90647282744369
Kaaterinka <katerinabolotina@inbox.ru>
2021-09-12 12:53:33 +0300

    Task done

--- io/src/main/kotlin/ru/sber/io/Archivator.kt
+++ io/src/main/kotlin/ru/sber/io/Archivator.kt
@@ -1,4 +1,7 @@
-package ru.sber.io
+package src.main.kotlin.ru.sber.io
+
+import java.io.*
+import java.util.zip.*
 
 /**
  * Реализовать методы архивации и разархивации файла.
@@ -10,15 +13,35 @@ class Archivator {
      * Метод, который архивирует файл logfile.log в архив logfile.zip.
      * Архив должен располагаться в том же каталоге, что и исходной файл.
      */
-    fun zipLogfile() {
-
+    fun zipLogfile(inputFileName: String, zipFileName: String) {
+        FileInputStream(inputFileName).use { input ->
+            ZipOutputStream(FileOutputStream(zipFileName)).use { output ->
+                val buff = ByteArray(input.available())
+                val entry = ZipEntry(inputFileName)
+                output.putNextEntry(entry)
+                input.read(buff)
+                output.write(buff)
+            }
+        }
     }
 
     /**
      * Метод, который извлекает файл из архива.
      * Извлечь из архива logfile.zip файл и сохарнить его в том же каталоге с именем unzippedLogfile.log
      */
-    fun unzipLogfile() {
-
+    fun unzipLogfile(zipFileName: String, originFileName: String) {
+        val buff = ByteArray(1024)
+        ZipInputStream(FileInputStream(zipFileName)).use { input ->
+            if (input.nextEntry != null) {
+                FileOutputStream(originFileName).use { output ->
+                    var tmp = input.read(buff)
+                    while (tmp != -1) {
+                        output.write(buff, 0, tmp)
+                        tmp = input.read(buff)
+                    }
+                    input.closeEntry()
+                }
+            }
+        }
     }
 }
