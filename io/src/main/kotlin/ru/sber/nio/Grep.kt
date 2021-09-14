1f7e7ce39d2227ab8804deced42b4f9f480c8c1b
Kaaterinka <katerinabolotina@inbox.ru>
2021-09-14 19:41:11 +0300

    Nio done

--- io/src/main/kotlin/ru/sber/nio/Grep.kt
+++ io/src/main/kotlin/ru/sber/nio/Grep.kt
@@ -1,5 +1,11 @@
 package ru.sber.nio
 
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.io.path.name
+import kotlin.io.path.useLines
+
 /**
  * Реализовать простой аналог утилиты grep с использованием калссов из пакета java.nio.
  */
@@ -15,6 +21,20 @@ class Grep {
      * 22-01-2001-1.log : 3 : 192.168.1.1 - - [22/Jan/2001:14:27:46 +0000] "POST /files HTTP/1.1" 200 - "-"
      */
     fun find(subString: String) {
+        val path = Paths.get("io/logs")
+        val result = File("io/result.txt")
 
+        result.outputStream().use { out ->
+            Files.walk(path).filter { it.toString().endsWith(".log") }.forEach { file ->
+                file.useLines {
+                    it.filter { s -> s.contains(subString, ignoreCase = true) }.forEach { str ->
+                        out.write((file.name + " : " + file.useLines { it.indexOf(str) + 1 } + " : " + str + "\n").toByteArray())
+                    }
+                }
+            }
+        }
     }
 }
+
+
+
